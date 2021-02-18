6fa76cdb6ace6bb7c09da58a574a323aa393731b
shubhanshugupt <gupta.shubhanshu05@gmail.com>
2021-02-18 12:29:24 +0530

    Used bit position macros to update register

--- drivers/Inc/stm32f407xx_spi_driver.h
+++ drivers/Inc/stm32f407xx_spi_driver.h
@@ -1,5 +1,5 @@
 /*
- * stm32f4xx_spi_driver.h
+ * stm32f407xx_spi_driver.h
  *
  *  Created on: 15-Feb-2021
  *      Author: shubh
@@ -12,13 +12,13 @@
 
 typedef struct
 {
-	uint8_t	SPI_DeviceMode;
-	uint8_t	SPI_BusConfig;
-	uint8_t	SPI_DFF;
-	uint8_t	SPI_CPHA;
-	uint8_t	SPI_CPOL;
-	uint8_t	SPI_SSM;
-	uint8_t	SPI_SCLKSpeed;
+	uint8_t	SPI_DeviceMode;				// Possible values from @SPI_DEVICE_MODE
+	uint8_t	SPI_BusConfig;				// Possible values from @SPI_BUS_CONFIGS
+	uint8_t	SPI_DFF;					// Possible values from @SPI_DATA_FRAME_FORMAT
+	uint8_t	SPI_CPHA;					// Possible values from @SPI_CPHA
+	uint8_t	SPI_CPOL;					// Possible values from @SPI_CPOL
+	uint8_t	SPI_SSM;					// Possible values from @SPI_SSM
+	uint8_t	SPI_SCLKSpeed;				// Possible values from SPI_SCLK_SPEED
 }SPI_PinConfig_t;
 
 typedef struct
@@ -27,6 +27,56 @@ typedef struct
 	SPI_PinConfig_t SPI_PinConfig;
 }SPI_Handle_t;
 
+
+/*--------------------------SPI Device Modes--------------------------------*/
+/*	@SPI_DEVICE_MODE	*/
+/*	SPI_CR1 bit2: MSTR	*/
+#define	SPI_MODE_SLAVE			0			//Default value
+#define	SPI_MODE_MASTER			1
+
+/*------------------------SPI Bus Configurations-----------------------------*/
+/*	@SPI_BUS_CONFIGS	*/
+/*	SPI_CR1 bit15,14,10	*/
+#define	SPI_BUS_FULL_DUP		1
+#define SPI_BUS_HALF_DUP		2
+#define	SPI_BUS_SIMPLEX_RX		3
+#define	SPI_BUS_SIMPLEX_TX		4
+
+/*-----------------------SPI Data Frame Format------------------------------*/
+/*	@SPI_DATA_FRAME_FORMAT	*/
+/*	SPI_CR1 bit11	*/
+#define SPI_DFF_8BIT			0			//Default value
+#define	SPI_DFF_16BIT			1
+
+/*-------------------------SPI SCLK Phase-----------------------------------*/
+/*	@SPI_CPHA	*/
+/*	SPI_CR1 bit0	*/
+#define	SPI_CPHA_FIRST_EDGE		0			//Default value
+#define	SPI_CPHA_SECOND_EDGE	1
+
+/*------------------------SPI SCLK Polarity---------------------------------*/
+/*	@SPI_CPOL	*/
+/*	SPI_CR1 bit1	*/
+#define SPI_CPOL_LOW			0			//Default value
+#define SPI_CPOL_HIGH			1
+
+/*----------------------SPI Slave Select Mgmt-------------------------------*/
+/*	@SPI_SSM	*/
+/*	SPI_CR1 bit8,9	*/
+#define SPI_SSM_DI				0			//Default value
+#define	SPI_SSM_EN				1
+
+/*-------------------------SPI SCLK Speed------------------------------------*/
+/*	@SPI_SCLK_SPEED	*/
+/*	SPI_CR1 bit 3,4,5	*/
+#define SPI_SCLK_BY_2			0			//Default value
+#define SPI_SCLK_BY_4			1
+#define SPI_SCLK_BY_8			2
+#define SPI_SCLK_BY_16			3
+#define SPI_SCLK_BY_32			4
+#define SPI_SCLK_BY_64			5
+#define SPI_SCLK_BY_128			6
+#define SPI_SCLK_BY_256			7
 
 /*-----------------------------------------------------------------------------
   					APIs supported by this Driver
--- drivers/Src/stm32f407xx_spi_driver.c
+++ drivers/Src/stm32f407xx_spi_driver.c
@@ -5,10 +5,22 @@
  *      Author: shubh
  */
 
-#include "stm32f407xx_gpio_driver.h"
+#include "stm32f407xx_spi_driver.h"
 
 
 //	Peripheral Clock Setup
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_PClkCtrl
+ *
+ * @brief		-	This function Enables or Disables the Peripheral clock for SPI
+ *
+ * @param[in]	-	base address of the SPI peripheral
+ * @param[in]	-	ENABLE or DISABLE macros, defined in MCU header file "stm32f407xx.h"
+ *
+ * @return		-	none
+ *
+ * @note		-	none
+ -----------------------------------------------------------------------------------------*/
 void SPI_PClkCtrl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 {
 	if (EnorDi == ENABLE)
@@ -43,35 +55,164 @@ void SPI_PClkCtrl(SPI_RegDef_t *pSPIx, uint8_t EnorDi)
 	}
 }
 
+
 //	SPI Init and DeInit
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_Init
+ *
+ * @brief		-	This function initializes the peripheral, ie, configures all the registers
+ *
+ * @param[in]	-	pointer to the handle structure
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-	none
+ -----------------------------------------------------------------------------------------*/
 void SPI_Init(SPI_Handle_t *pSPIHandle)
 {
+	uint32_t tempreg = 0;
 
+	//1. Configure the device modes
+	tempreg |= ( (pSPIHandle->SPI_PinConfig.SPI_DeviceMode) << SPI_CR1_MSTR );
+
+	//2. Configure the BUS Config
+	if (pSPIHandle->SPI_PinConfig.SPI_BusConfig == SPI_BUS_FULL_DUP)
+	{
+		//Use unidirectional mode, ie, Clear BIDIMODE
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE );
+	}
+	else if(pSPIHandle->SPI_PinConfig.SPI_BusConfig == SPI_BUS_HALF_DUP)
+	{
+		//Set BIDIMODE
+		tempreg |= (1 << SPI_CR1_BIDIMODE);
+	}
+	else if(pSPIHandle->SPI_PinConfig.SPI_BusConfig == SPI_BUS_SIMPLEX_RX)
+	{
+		//Clear BIDIMODE
+		tempreg &= ~(1 << SPI_CR1_BIDIMODE);
+		//RXONLY bit must be set
+		tempreg |= (1 << SPI_CR1_RXONLY);
+	}
+
+	//3. Configure the Data Frame Format
+	tempreg |= ( (pSPIHandle->SPI_PinConfig.SPI_DFF) << SPI_CR1_DFF);
+
+	//4. Configure the Clock Phase
+	tempreg |= ( (pSPIHandle->SPI_PinConfig.SPI_CPHA) << SPI_CR1_CPHA);
+
+	//5. Configure the Clock Polarity
+	tempreg |= ( (pSPIHandle->SPI_PinConfig.SPI_CPOL) << SPI_CR1_CPOL);
+
+	//6. Configure Slave Select Mode
+	tempreg |= ( (pSPIHandle->SPI_PinConfig.SPI_SSM) << SPI_CR1_SSM);
+
+	//7. Configure Serial Clock Speed
+	tempreg |= ( (pSPIHandle->SPI_PinConfig.SPI_SCLKSpeed) << SPI_CR1_BR);
+
+	pSPIHandle->pSPIx->CR1 = tempreg;
 }
+
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_DeInit
+ *
+ * @brief		-	It resets the SPI peripheral registers
+ *
+ * @param[in]	-	Address of the peripheral
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-	We have a special register in RCC peripheral, called AHB1 peripheral reset register
+ * 					This will reset all the peripheral registers
+ -----------------------------------------------------------------------------------------*/
 void SPI_DeInit(SPI_RegDef_t *pSPIx)
 {
 
 }
 
 //	Data Send and Receive
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_DataSend
+ *
+ * @brief		-
+ *
+ * @param[in]	-	Address of the peripheral
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-
+ -----------------------------------------------------------------------------------------*/
 void SPI_DataSend(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t DataLen)
 {
 
 }
+
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_DataSend
+ *
+ * @brief		-
+ *
+ * @param[in]	-	Address of the peripheral
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-
+ -----------------------------------------------------------------------------------------*/
 void SPI_DataReceive(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t DataLen)
 {
 
 }
 
 //	IRQ Configuration and ISR Handling
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_DataSend
+ *
+ * @brief		-
+ *
+ * @param[in]	-	Address of the peripheral
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-
+ -----------------------------------------------------------------------------------------*/
 void SPI_IRQConfig(uint8_t IRQNumber, uint8_t EnorDi)
 {
 
 }
+
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_DataSend
+ *
+ * @brief		-
+ *
+ * @param[in]	-	Address of the peripheral
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-
+ -----------------------------------------------------------------------------------------*/
 void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
 {
 
 }
+
+/*----------------------------------------------------------------------------------------
+ * @fn			-	SPI_DataSend
+ *
+ * @brief		-
+ *
+ * @param[in]	-	Address of the peripheral
+ * @param[in]	-	none
+ *
+ * @return		-	none
+ *
+ * @note		-
+ -----------------------------------------------------------------------------------------*/
 void SPI_IRQHandling(SPI_Handle_t *pSPIHandle)
 {
 
