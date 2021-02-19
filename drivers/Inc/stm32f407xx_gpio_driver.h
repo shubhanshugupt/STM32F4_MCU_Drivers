4b73464856bc1033ad1890879d5eb0c6650d8227
shubhanshugupt <gupta.shubhanshu05@gmail.com>
2021-02-19 12:20:05 +0530

    Updated Alternate Functionality MACROS

--- drivers/Inc/stm32f407xx_gpio_driver.h
+++ drivers/Inc/stm32f407xx_gpio_driver.h
@@ -17,7 +17,7 @@ typedef struct
 	uint8_t GPIO_PinSpeed;			// Possible values from @GPIO_PIN_SPEEDS
 	uint8_t GPIO_PinPuPdControl;	// Possible values from @GPIO_PIN_PUPD_CTRL
 	uint8_t GPIO_PinOpType;			// Possible values from @GPIO_PIN_OP_TYPE
-	uint8_t GPIO_PinAltFuncMode;
+	uint8_t GPIO_PinAltFuncMode;	// Possible values from @GPIO_PIN_ALT_FUNC
 }GPIO_PinConfig_t;
 
 typedef struct
@@ -77,6 +77,23 @@ typedef struct
 #define GPIO_PIN_PU			1		//Pull up
 #define GPIO_PIN_PD			2		//Pull down
 
+/*-------------GPIO PIN alternate functionalities---------------------------------*/
+#define AF0					0
+#define AF1					1
+#define AF2					2
+#define AF3					3
+#define AF4					4
+#define AF5					5
+#define AF6					6
+#define AF7					7
+#define AF8					8
+#define AF9					9
+#define AF10				10
+#define AF11				11
+#define AF12				12
+#define AF13				13
+#define AF14				14
+#define AF15				15
 
 
 
